ba91acc1a23ebe4b06a8debf17e8d741ef293f17
Mungi Cheon <c.mungi7421@gmail.com>
2025-11-25 15:50:51 +0900

    Kafka Stream 데이터 조회 Service, Controller 구현

---
+++ src/main/kotlin/org/kafka_lecture/controller/StatsController.kt
@@ -0,0 +1,27 @@
+package org.kafka_lecture.controller
+
+import org.kafka_lecture.model.OrderCountComparisonStats
+import org.kafka_lecture.model.StatsResponse
+import org.kafka_lecture.stream.OrderStreamsService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/stats")
+class StatsController(
+    private val orderStreamsService: OrderStreamsService
+) {
+
+    @GetMapping("/orders/count")
+    fun getOrderCountStats(): ResponseEntity<StatsResponse<OrderCountComparisonStats>> {
+        val stats = orderStreamsService.orderCountComparison()
+        val response = StatsResponse(
+            success = true,
+            data = stats,
+            message = "Order count statistics retrieved successfully"
+        )
+        return ResponseEntity.ok(response)
+    }
+}
