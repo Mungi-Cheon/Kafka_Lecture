d61cd2c1ee0058a1a7fe403eaccb74e68deb9847
Mungi Cheon <c.mungi7421@gmail.com>
2025-11-25 14:19:55 +0900

    kafka 초기 설정 구현

---
+++ src/main/kotlin/org/kafka_lecture/config/KafkaConfig.kt
@@ -0,0 +1,150 @@
+package org.kafka_lecture.config
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.kafka_lecture.model.OrderEvent
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.*
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+class KafkaConfig {
+
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private lateinit var bootstrapServers: String
+
+    companion object {
+        const val SCHEMA_REGISTRY_URL = "http://localhost:8081"
+    }
+
+    @Bean
+    fun orderEventConsumerFactory(): ConsumerFactory<String, OrderEvent>{
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java, // 직렬화
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java, // 역직렬화
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to StringDeserializer::class.java,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to JsonDeserializer::class.java,
+            JsonDeserializer.VALUE_DEFAULT_TYPE to OrderEvent::class.java,
+            JsonDeserializer.TRUSTED_PACKAGES to "*",
+            JsonDeserializer.USE_TYPE_INFO_HEADERS to false
+//            ConsumerConfig.GROUP_ID_CONFIG to UUID.randomUUID().toString(),
+        )
+
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun orderEventKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, OrderEvent> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, OrderEvent>()
+        factory.consumerFactory = orderEventConsumerFactory()
+        return factory
+    }
+
+    @Bean
+    fun genericConsumerFactory(): ConsumerFactory<String, Any> {
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to StringDeserializer::class.java,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to JsonDeserializer::class.java,
+            JsonDeserializer.TRUSTED_PACKAGES to "*",
+            JsonDeserializer.USE_TYPE_INFO_HEADERS to true
+        )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun genericKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = genericConsumerFactory()
+        return factory
+    }
+
+    @Bean
+    fun cdcConsumerFactory(): ConsumerFactory<String, String> {
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.GROUP_ID_CONFIG to "real-order-cdc-processor",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to true,
+            ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG to 1000
+        )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun avroConsumerFactory(): ConsumerFactory<String, GenericRecord> {
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to SCHEMA_REGISTRY_URL,
+            KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG to false,
+            AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS to true
+        )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun avroKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, GenericRecord> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, GenericRecord>()
+        factory.consumerFactory = avroConsumerFactory()
+        return factory
+    }
+
+    //// --------- Producer -------------
+
+    @Bean
+    fun orderEventProducerFactory(): ProducerFactory<String, OrderEvent> {
+        val props = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
+            JsonSerializer.ADD_TYPE_INFO_HEADERS to true
+        )
+        return DefaultKafkaProducerFactory(props)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, OrderEvent> {
+        return KafkaTemplate(orderEventProducerFactory())
+    }
+
+    @Bean
+    fun avroProducerFactory(): ProducerFactory<String, GenericRecord> {
+        val props = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
+            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to SCHEMA_REGISTRY_URL,
+            AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS to true,
+            ProducerConfig.ACKS_CONFIG to "1", // "0" : 확인X, "1" : Leader에게만 전송확인, "all" : 복제본이 있다면 복제본까지 확인
+            ProducerConfig.RETRIES_CONFIG to 3,
+            ProducerConfig.BATCH_SIZE_CONFIG to 16384,
+            ProducerConfig.LINGER_MS_CONFIG to 10,
+            ProducerConfig.COMPRESSION_TYPE_CONFIG to "snappy" // 압축 알고리즘 설정, none, lz4, gzip, zstd 알고리즘 존재
+        )
+        return DefaultKafkaProducerFactory(props)
+    }
+
+    @Bean
+    fun avroKafkaTemplate(): KafkaTemplate<String, GenericRecord> {
+        return KafkaTemplate(avroProducerFactory())
+    }
+}
